401a0c47a7149547d8e6b38a06be1ffb99950f2b
onedrako <onedrako@gmail.com>
2021-12-09 11:02:23 -0600

    :clown_face: alert image not working
    changed alert image

--- src/components/Discord/RequiredMessage.jsx
+++ src/components/Discord/RequiredMessage.jsx
@@ -5,7 +5,7 @@ import { Img, P, Div } from '../../styles/Discord/StyledErrorMessage'
 const RequiredMessage = ({ message }) => {
   return (
     <Div>
-      <Img src='https://cdn-icons.flaticon.com/png/512/559/premium/559343.png?token=exp=1638835247~hmac=bca88333bef6a44c5a2faf20d7295979' alt='Error Icon' />
+      <Img src='https://cdn-icons.flaticon.com/png/512/559/premium/559384.png?token=exp=1639070150~hmac=6ab3df420bcc70d88237b256f11e1e1c' alt='Error Icon' />
       <P>{message}</P>
     </Div>
   )
