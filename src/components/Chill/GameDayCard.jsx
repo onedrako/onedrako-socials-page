837a0c50676f6d6e4722909aaf122de02964ae41
onedrako <onedrako@gmail.com>
2022-02-11 07:09:21 -0600

    Basic behavior of Cards finished, try to improve the ticket and hover behavior

--- src/components/Chill/GameDayCard.jsx
+++ src/components/Chill/GameDayCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import { GameDayCardContainer, CardContainer, MainInfoContainer, SponsorIcon } from '../../styles/Chill/GameCardDay'
+import { GameDayCardContainer, CardContainer, MainInfoContainer, SponsorIcon, ExtraInfo } from '../../styles/Chill/GameCardDay'
 
 const GameDayCard = () => {
+  const [extraInfo, setExtraInfo] = React.useState(false)
   return (
     <>
       <GameDayCardContainer>
-        <CardContainer>
+
+        <CardContainer onClick={() => setExtraInfo(!extraInfo)}>
           <img src='https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/wiiu_14/SI_WiiU_MarioKart8_image1600w.jpg' />
           <MainInfoContainer>
             <h3>Lunes 10 de febrero</h3>
@@ -17,9 +19,23 @@ const GameDayCard = () => {
           </MainInfoContainer>
         </CardContainer>
 
-        <div>
-          <h2 />
-        </div>
+        {extraInfo
+          ? <ExtraInfo>
+            <h2>Mario Kart 8</h2>
+            <p>Jugando desde que salio este hermoso juego, en este si me defiendo</p>
+
+            <div>
+              <p>Plataformas: </p>
+              <img src='https://1000marcas.net/wp-content/uploads/2020/02/logo-Wii-U.png' alt='' />
+            </div>
+
+            <p>Inicia: 19:30</p>
+            <p>Termina: 21:30</p>
+
+          </ExtraInfo>
+
+          : null}
+
       </GameDayCardContainer>
     </>
   )
--- src/styles/Chill/GameCardDay.js
+++ src/styles/Chill/GameCardDay.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { ImTicket } from 'react-icons/im'
 
 const GameDayCardContainer = styled.div`
   display: flex;
+  flex-direction: column;
   place-items: center;
-  
+  margin : 0 auto;
+  width : 100%;
   `
 
 const CardContainer = styled.div`
@@ -13,16 +15,22 @@ const CardContainer = styled.div`
   width: 80%;
   margin: 0 auto;
   position: relative;
+  z-index: 1;
   margin-top: 20px;
+  filter: grayscale(100%);
   cursor: pointer;
+  & :hover{
+      transition: 0.5s;
+      transform: scale(1.05);
+    }
   & img{
     border-radius: 0 30px 0 0;
-  }
-  & :hover{
-    transition: all 0.5s ease-in-out;
-    transform: scale(1.05);
+    width: 100%;
+    height: 120px;
+    object-fit: cover;
   }
   `
+
 const MainInfoContainer = styled.div`
     display: flex;
     height: 35%;
@@ -31,9 +39,11 @@ const MainInfoContainer = styled.div`
     align-items: center;
     padding: 0px 20px;
     position: absolute;
+    z-index: 2;
     bottom : 0;
     background-color: rgba(0,0,0,0.6);
     font-size: 1rem;
+    /* pointer-events:none; */
     & div {
       display: flex;
       place-items: center;
@@ -41,17 +51,90 @@ const MainInfoContainer = styled.div`
   }
   `
 
+const MoveTicket = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  25%{
+    transform: rotate(15deg);
+  }
+
+  50%{
+    transform: rotate(-15deg);
+  }
+
+  75%{
+    transform: rotate(15deg);
+  }
+  100%{
+    transform: rotate(0deg);
+  }
+
+`
+
 const SponsorIcon = styled(ImTicket)`
   margin-right: 5px;
   color: #d8b100;
+  font-size: 1.2rem;
+  & :hover{
+    animation: ${MoveTicket} 1s ;
+  }
 `
 
-const MainInfo = styled.div`
+const appear = keyframes`
+  from {
+    transform : translateY(-40%);
+  }
+  to {
+    transform : translateY(0);
+  }
 `
+
+const ExtraInfo = styled.div`
+  display: grid;
+  gap: 15px;
+  width: 80%;
+  align-items: center;
+  background-color: #171717;
+  padding: 20px;
+  text-align: center;
+  border-radius: 0 0 30px 30px;
+  animation: ${appear} 1s ease-in-out;
+  & div {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    margin : 10px auto;
+    width: 90%;
+    margin-right: 10px;
+    & img {
+      display: block;
+      justify-self: center;
+      width: 20%;
+      margin: 5px 0;
+    }
+  }
+  & p {
+    font-size: 1rem;
+  }
+`
+
+// const NotAvailable = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   border-radius: 0 29px 0 0 ;
+//   position: absolute;
+//   z-index: 1;
+//   background-color: black;
+//   opacity: 0.7;
+// `
 
 export {
   GameDayCardContainer,
   CardContainer,
   MainInfoContainer,
-  SponsorIcon
+  SponsorIcon,
+  // NotAvailable,
+
+  ExtraInfo
 }
