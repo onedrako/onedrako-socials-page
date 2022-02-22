a139d5b19a437f0ff8c17100b566c4d242d15117
onedrako <onedrako@gmail.com>
2022-02-22 13:04:01 -0600

    V1 ready for deploy, think are style that can be better but all its working with dates and countdown

--- src/utils/defineTimesForChillSection.js
+++ src/utils/defineTimesForChillSection.js
@@ -2,6 +2,7 @@ import { defineToday } from '../utils/defineToday'
 import moment from 'moment-timezone'
 
 const defineTimesForChillSection = (data) => {
+  let dateToCountDown
   const today = defineToday(data)
 
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions()
@@ -11,7 +12,32 @@ const defineTimesForChillSection = (data) => {
   const initialTimeForUserCountry = moment.tz(myStartStreamAreaInMexicoCity, userTimeZone.timeZone).format('HH:mm')
   const endTimeForUserCountry = moment.tz(myEndStreamAreaInMexicoCity, userTimeZone.timeZone).format('HH:mm')
 
-  const dateToCountDown = moment.tz(myStartStreamAreaInMexicoCity, userTimeZone.timeZone).format()
+  // DEFINE COUNTDOWN
+  if (today[0].state === 'true') {
+    dateToCountDown = moment.tz(myStartStreamAreaInMexicoCity, userTimeZone.timeZone).format()
+  } else {
+    let addDays = 0
+    let status = false
+
+    const nextDaysFromToday = data.filter(gameDay => {
+      const { date } = gameDay
+      return date > today[0].date
+    }).sort((a, b) => {
+      return moment(a.date) - moment(b.date)
+    })
+
+    while (!status && addDays < nextDaysFromToday.length) {
+      nextDaysFromToday[addDays].state === false
+        ? addDays++
+        : status = true
+    }
+
+    if (addDays === 0) {
+      dateToCountDown = moment.tz(myEndStreamAreaInMexicoCity, userTimeZone.timeZone).add(1, 'days').format()
+    } else {
+      dateToCountDown = moment.tz(myEndStreamAreaInMexicoCity, userTimeZone.timeZone).add(addDays + 1, 'days').format()
+    }
+  }
 
   return {
     userTimeZone: userTimeZone.timeZone,
